8d1d00b9fd56588326067097a07771a18e72f750
karthik <karthikeyayadav547@gmail.com>
2026-06-11 18:15:11 +0530

    fix asset path for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import heroImg from './assets/Hero.png'
 import './App.css'
 import MainLayout from './Layout/MainLayout'
 
